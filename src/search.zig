44a318f3a8a75c578539ea8f386aa653d7406879
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-01-03 15:29:06 +0100

    non functional, prep for pesto
    bench: 31565087

--- src/search.zig
+++ src/search.zig
@@ -20,13 +20,66 @@ const shouldStopSearching = engine.shouldStopSearching;
 
 const max_search_depth = 255;
 
-fn search(comptime turn: Side, board: *Board, cur_depth: u8, depth_remaining: u8, alpha_inp: i16, beta: i16, move_buf: []Move, hash_history: *std.ArrayList(u64)) anyerror!struct { i16, Move } {
+fn quiesce(comptime turn: Side, board: *Board, cur_depth: u8, alpha_inp: i16, beta: i16, move_buf: []Move) i16 {
+    var alpha = alpha_inp;
+    qnodes += 1;
+    if (cur_depth > 0 and qnodes % 1024 == 0 and (shouldStopSearching() or timer.read() >= hard_time)) {
+        shutdown = true;
+        return 0;
+    }
+    const move_count = movegen.getCaptures(turn, board.*, move_buf);
+    const static_eval = evaluate(board.*);
+    if (move_count == 0) {
+        return static_eval;
+    }
+
+    if (static_eval >= beta) return beta;
+    if (static_eval > alpha) alpha = static_eval;
+
+    move_ordering.mvvLva(board, move_buf[0..move_count]);
+    var best_score = static_eval;
+    for (move_buf[0..move_count]) |move| {
+        if (!move.isCapture()) return -checkmate_score;
+        const inv = board.playMove(turn, move);
+        defer board.undoMove(turn, inv);
+
+        const score = -quiesce(
+            turn.flipped(),
+            board,
+            cur_depth + 1,
+            -beta,
+            -alpha,
+            move_buf[move_count..],
+        );
+        if (shutdown)
+            break;
+
+        if (score > best_score) {
+            best_score = score;
+        }
+        if (score > alpha) {
+            if (score >= beta) {
+                break;
+            }
+            alpha = score;
+        }
+    }
+    return best_score;
+}
+
+fn search(comptime root: bool, comptime turn: Side, board: *Board, cur_depth: u8, depth_remaining: u8, alpha_inp: i16, beta: i16, move_buf: []Move, hash_history: *std.ArrayList(u64)) error{EarlyShutdown}!if (root) struct { i16, Move } else i16 {
+    const result = struct {
+        inline fn impl(score: i16, move: Move) if (root) struct { i16, Move } else i16 {
+            return if (root) .{ score, move } else score;
+        }
+    }.impl;
     var alpha = alpha_inp;
     nodes += 1;
     if (cur_depth > 0 and nodes % 1024 == 0 and (shouldStopSearching() or timer.read() >= hard_time)) return error.EarlyShutdown;
     const move_count, const masks = movegen.getMovesWithInfo(turn, false, board.*, move_buf);
+    const is_in_check = masks.checks != 0;
     if (move_count == 0) {
-        return .{ if (masks.checks == 0) 0 else eval.mateIn(cur_depth), Move.null_move };
+        return result(if (is_in_check) eval.mateIn(cur_depth) else 0, Move.null_move);
     }
 
     {
@@ -36,10 +89,16 @@ fn search(comptime turn: Side, board: *Board, cur_depth: u8, depth_remaining: u8
                 repetitions += 1;
             }
         }
-        if (repetitions >= 3) return .{ 0, Move.null_move };
+        if (repetitions >= 3) return result(0, move_buf[0]);
     }
 
-    if (depth_remaining == 0) return .{ evaluate(board.*), move_buf[0] };
+    if (depth_remaining == 0) {
+        return result(evaluate(board.*), move_buf[0]);
+        // const score = quiesce(turn, board, cur_depth, alpha_inp, beta, move_buf);
+        // if (shutdown)
+        //     return error.EarlyShutdown;
+        // return result(score, move_buf[0]);
+    }
 
     move_ordering.mvvLva(board, move_buf[0..move_count]);
     var best_score = -checkmate_score;
@@ -50,7 +109,8 @@ fn search(comptime turn: Side, board: *Board, cur_depth: u8, depth_remaining: u8
         hash_history.appendAssumeCapacity(board.zobrist);
         defer _ = hash_history.pop();
 
-        const tmpscore, _ = try search(
+        const score = -(try search(
+            false,
             turn.flipped(),
             board,
             cur_depth + 1,
@@ -59,8 +119,7 @@ fn search(comptime turn: Side, board: *Board, cur_depth: u8, depth_remaining: u8
             -alpha,
             move_buf[move_count..],
             hash_history,
-        );
-        const score = -tmpscore;
+        ));
 
         if (score > best_score) {
             best_score = score;
@@ -77,11 +136,13 @@ fn search(comptime turn: Side, board: *Board, cur_depth: u8, depth_remaining: u8
             alpha = score;
         }
     }
-    return .{ best_score, best_move };
+    return result(best_score, best_move);
 }
 
 pub fn reset() void {
     nodes = 0;
+    qnodes = 0;
+    shutdown = false;
 }
 
 pub fn iterativeDeepening(board: Board, search_params: engine.SearchParameters, move_buf: []Move, hash_history: *std.ArrayList(u64), silence_output: bool) !engine.SearchResult {
@@ -95,7 +156,7 @@ pub fn iterativeDeepening(board: Board, search_params: engine.SearchParameters, 
     var move = Move.null_move;
     for (0..search_params.maxDepth()) |depth| {
         score, move = switch (board.turn) {
-            inline else => |turn| search(turn, &board_copy, 0, @intCast(depth), -checkmate_score, checkmate_score, move_buf, hash_history),
+            inline else => |turn| search(true, turn, &board_copy, 0, @intCast(depth), -checkmate_score, checkmate_score, move_buf, hash_history),
         } catch break;
         if (!silence_output) {
             write("info depth {} score cp {} nodes {} nps {} time {} pv {}\n", .{
@@ -120,12 +181,14 @@ pub fn iterativeDeepening(board: Board, search_params: engine.SearchParameters, 
         .score = score,
         .stats = engine.SearchStatistics{
             .nodes = nodes,
-            .qnodes = 0,
+            .qnodes = qnodes,
             .ns_used = timer.read(),
         },
     };
 }
 
 var nodes: u64 = 0;
+var qnodes: u64 = 0;
 var timer: std.time.Timer = undefined;
+var shutdown = false;
 var hard_time: u64 = 0;
