60032cc2944b40e5dff915529605717fdfac1be1
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-01-30 19:52:08 +0100

    lower threshold for see pruning in qsearch
    bench: 5107117

--- src/tuning.zig
+++ src/tuning.zig
@@ -2,7 +2,7 @@ const do_tuning = false;
 
 pub const tunable_constants = if (do_tuning) struct {
     pub var rfp_multiplier: i16 = 150;
-    pub var quiesce_see_pruning_threshold: i16 = -100;
+    pub var quiesce_see_pruning_threshold: i16 = 0;
 
     pub var see_quiet_pruning_multiplier: i16 = -80;
     pub var see_noisy_pruning_multiplier: i16 = -40;
@@ -12,7 +12,7 @@ pub const tunable_constants = if (do_tuning) struct {
 } else struct {
     pub const rfp_multiplier: i16 = 150;
 
-    pub const quiesce_see_pruning_threshold: i16 = -100;
+    pub const quiesce_see_pruning_threshold: i16 = 0;
     pub const see_quiet_pruning_multiplier: i16 = -80;
     pub const see_noisy_pruning_multiplier: i16 = -40;
 
