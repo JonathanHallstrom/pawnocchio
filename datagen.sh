b98900c57c3606941de89c9f414c154d11fa5c29
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-02-12 22:13:03 +0100

    add datagen scripts
    bench: 5448620

--- datagen.sh
+++ datagen.sh
@@ -1,9 +1,7 @@
+echo $1
 zig build-exe -OReleaseFast src/datagen.zig
-time bash -c 'for i in {1..32}; do
-    ./datagen -o out$i.vf -g 100 -n 5000 -r 4 &
+for i in $(seq 1 $1); do
+    echo starting $i
+    ./datagen -o out$i.vf -g 3000_000 -n 5000 -r 4 &
 done
-wait'
-for i in {1..32}; do
-    echo out$i.bin
-    /home/jonathanhallstrom/dev/rust/viridithas/target/release/viridithas splat ~/dev/zig/pawnocchio/out$i.vf out$i.bin
-done
+wait
---
+++ splat.sh
@@ -0,0 +1,16 @@
+echo $1
+for i in $(seq 1 $1); do
+    rm -fr tmp$1.bin
+    (/home/jonathanhallstrom/dev/rust/viridithas/target/release/viridithas splat --cfg-path=4pc.toml --pgn ~/dev/zig/pawnocchio/out$i.vf tmp$1.bin && 
+     /home/jonathanhallstrom/dev/rust/viridithas/target/release/viridithas splat --cfg-path=4pc.toml ~/dev/zig/pawnocchio/out$i.vf out$i.bin) &
+    rm -fr tmp$1.bin
+done
+wait
+
+
+for i in $(seq 1 $1); do
+    /home/jonathanhallstrom/dev/rust/bullet/target/release/bullet-utils shuffle --mem-used-mb 1024 --input out$i.bin --output shuffled$i.bin
+done
+
+
+/home/jonathanhallstrom/dev/rust/bullet/target/release/bullet-utils interleave shuffled?*bin --output shuffled.bin
