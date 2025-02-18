b07dde9ec4d373569b0a6188e0dd35f77ff077ca
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-02-18 11:13:42 +0100

    double extend when singular search result is much below singular beta
    bench: 6418153

--- src/search.zig
+++ src/search.zig
@@ -300,6 +300,8 @@ fn search(
             const score: i16 = search(false, turn, pv, board, eval_state, s_beta - 1, s_beta, cur_depth, s_depth, move_buf[move_count..], previous_move, move, hash_history) orelse 0;
             if (score < s_beta)
                 extension += 1;
+            if (score < s_beta - 20)
+                extension += 1;
         }
 
         const updated_eval_state = eval_state.updateWith(turn, board, move);
