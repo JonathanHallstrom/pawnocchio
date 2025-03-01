ef52a06b52ff69ea01a8688ed0efadccc60ea187
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-03-01 16:28:09 +0100

    take 3
    bench: 2627920

--- src/correction.zig
+++ src/correction.zig
@@ -5,21 +5,21 @@ const eval = @import("eval.zig");
 // heavily based on https://github.com/Ciekce/Stormphrax/blob/main/src/correction.h
 
 pub fn update(board: *const Board, corrected_static_eval: i16, score: i16, depth: u8) void {
-    const err: i32 = @as(i32, score) - corrected_static_eval;
-    const weight: i16 = @min(depth, 15) + 1;
+    const err: i32 = (@as(i32, score) - corrected_static_eval) * 256;
+    const weight: i32 = @min(depth, 15) + 1;
 
     updatePawnCorrhist(board, err, weight);
 }
 
-fn updatePawnCorrhist(board: *const Board, err: anytype, weight: i16) void {
+fn updatePawnCorrhist(board: *const Board, err: i32, weight: i32) void {
     const entry = &pawn_corrhist[board.pawn_zobrist % pawn_corrhist.len];
-    const lerped = (entry.* * @as(i32, 256 - weight) + err * weight) >> 8;
+    const lerped = (entry.* * (256 - weight) + err * weight) >> 8;
     const clamped = std.math.clamp(lerped, -max_history, max_history);
     entry.* = @intCast(clamped);
 }
 
 pub fn correct(board: *const Board, static_eval: i16) i16 {
-    const correction: i32 = pawn_corrhist[board.pawn_zobrist % pawn_corrhist.len] >> 3;
+    const correction: i32 = pawn_corrhist[board.pawn_zobrist % pawn_corrhist.len] >> 8;
     return eval.clampScore(static_eval + correction);
 }
 
