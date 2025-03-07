a8a55595c75eb704691a4600bff30c2a52e94bca
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-03-07 23:21:49 +0100

    add new build script
    bench: 3150818

---
+++ build_release.py
@@ -0,0 +1,44 @@
+import os
+
+VERSION = "1.3"
+
+def run_build(command):
+    print(f"Running: {command}")
+    os.system(command)
+
+builds = [
+    ("x86", "windows", "i386", None),
+    ("x86_64", "windows", "x86_64", None),
+    ("x86_64", "windows", "x86_64", "x86_64_v2"),
+    ("x86_64", "windows", "x86_64", "x86_64_v3"),
+    ("x86_64", "windows", "x86_64", "x86_64_v4"),
+    ("x86_64", "windows", "x86_64", "znver1"),
+    ("x86_64", "windows", "x86_64", "znver2"),
+    ("x86_64", "windows", "x86_64", "znver3"),
+    ("x86_64", "windows", "x86_64", "znver4"),
+    ("x86_64", "windows", "x86_64", "znver5"),
+    ("aarch64", "windows", "aarch64", None),
+
+    ("x86", "linux", "i386", None),
+    ("x86_64", "linux", "x86_64", None),
+    ("x86_64", "linux", "x86_64", "x86_64_v2"),
+    ("x86_64", "linux", "x86_64", "x86_64_v3"),
+    ("x86_64", "linux", "x86_64", "x86_64_v4"),
+    ("x86_64", "linux", "x86_64", "znver1"),
+    ("x86_64", "linux", "x86_64", "znver2"),
+    ("x86_64", "linux", "x86_64", "znver3"),
+    ("x86_64", "linux", "x86_64", "znver4"),
+    ("x86_64", "linux", "x86_64", "znver5"),
+    ("aarch64", "linux", "aarch64", None),
+
+    ("x86_64", "macos", "x86_64", None),
+    ("aarch64", "macos", "aarch64", None),
+]
+
+for arch, os_name, zig_arch, cpu in builds:
+    cpu_flag = f"-Dcpu={cpu} " if cpu else ""
+    output_name_base = f"pawnocchio-{VERSION}-{os_name}-{arch}"
+    output_name = output_name_base + (".exe" if os_name == "windows" else "")
+    build_cmd = f"zig build --release=fast {cpu_flag}-Dtarget={arch}-{os_name} -Dname={output_name_base}"
+    move_cmd = f"mv zig-out/bin/{output_name} builds/{output_name}"
+    run_build(build_cmd + " && " + move_cmd)
