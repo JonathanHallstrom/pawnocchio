8e3ae0e26f95ef6835a4bc92653b226e74e0292e
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-03-08 12:31:48 +0100

    6 random plies
    bench: 3150818

--- datagen.sh
+++ datagen.sh
@@ -2,6 +2,6 @@ echo $1
 zig build-exe -OReleaseFast src/datagen.zig
 for i in $(seq 1 $1); do
     echo starting $i
-    ./datagen -o out$i.vf -g 3000_000 -n 5000 -r 4 &
+    ./datagen -o out$i.vf -g 3000_000 -n 5000 -r 6 &
 done
 wait
