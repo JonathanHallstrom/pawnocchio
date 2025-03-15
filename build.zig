24588585925480ccddb015a689c2a0463f80aca6
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-03-15 22:00:44 +0100

    update net
    bench: 1138058

--- build.zig
+++ build.zig
@@ -35,7 +35,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
 
     const name = b.option([]const u8, "name", "Change the name of the binary") orelse "pawnocchio";
-    const net = b.option([]const u8, "net", "Change the net to be used") orelse "net17_04_768_400_8_mirrored_2.nnue";
+    const net = b.option([]const u8, "net", "Change the net to be used") orelse "net18_04_768_400_8_mirrored_2.nnue";
     copyNetwork(net) catch |e| std.debug.panic("copying neural net failed with error: {}\n", .{e});
 
     // Standard optimization options allow the person running `zig build` to select
