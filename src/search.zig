296baab5bb49c42d8a015d16f932c0fb5bb53712
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-03-27 11:42:20 +0100

    in between failhard and failsoft
    bench: 1377281

--- src/search.zig
+++ src/search.zig
@@ -315,7 +315,7 @@ fn search(
         // reverse futility pruning
         // this is basically the same as what we do in qsearch, if the position is too good we're probably not gonna get here anyway
         if (depth <= 5 and tt_corrected_eval >= beta + tunable_constants.rfp_mult * (depth -| @intFromBool(improving))) {
-            return result(tt_corrected_eval, move_buf[0]);
+            return result(beta + @divFloor(tt_corrected_eval - beta, 3), move_buf[0]);
         }
 
         // razoring
