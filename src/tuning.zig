8aa156e9b6cb7d09f0e6314d335f9cb1ce69b8b7
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-05-11 16:50:43 +0200

    reduce more if we have a ttmove
    bench: 4707351

--- src/tuning.zig
+++ src/tuning.zig
@@ -68,6 +68,7 @@ const tunable_defaults = struct {
     pub const lmr_quiet_history_mult: i32 = 1023;
     pub const lmr_noisy_history_mult: i32 = 1019;
     pub const lmr_corrhist_mult: i32 = 11942;
+    pub const lmr_ttmove_mult: i32 = 512;
     pub const nmp_base: i32 = 38851;
     pub const nmp_mult: i32 = 1125;
     pub const nmp_eval_reduction_scale: i32 = 30;
@@ -120,6 +121,7 @@ pub const tunables = [_]Tunable{
     .{ .name = "lmr_quiet_history_mult", .default = tunable_defaults.lmr_quiet_history_mult },
     .{ .name = "lmr_noisy_history_mult", .default = tunable_defaults.lmr_noisy_history_mult },
     .{ .name = "lmr_corrhist_mult", .default = tunable_defaults.lmr_corrhist_mult },
+    .{ .name = "lmr_ttmove_mult", .default = tunable_defaults.lmr_ttmove_mult },
     .{ .name = "nmp_base", .default = tunable_defaults.nmp_base },
     .{ .name = "nmp_mult", .default = tunable_defaults.nmp_mult },
     .{ .name = "nmp_eval_reduction_scale", .default = tunable_defaults.nmp_eval_reduction_scale },
@@ -172,6 +174,7 @@ pub const tunable_constants = if (do_tuning) struct {
     pub var lmr_quiet_history_mult = tunable_defaults.lmr_quiet_history_mult;
     pub var lmr_noisy_history_mult = tunable_defaults.lmr_noisy_history_mult;
     pub var lmr_corrhist_mult = tunable_defaults.lmr_corrhist_mult;
+    pub var lmr_ttmove_mult = tunable_defaults.lmr_ttmove_mult;
     pub var nmp_base = tunable_defaults.nmp_base;
     pub var nmp_mult = tunable_defaults.nmp_mult;
     pub var nmp_eval_reduction_scale = tunable_defaults.nmp_eval_reduction_scale;
