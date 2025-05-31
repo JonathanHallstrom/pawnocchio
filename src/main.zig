ca3c065918895416a0695c751085a00a239a0066
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-05-31 16:59:15 +0200

    bump version
    bench: 3212514

--- src/main.zig
+++ src/main.zig
@@ -20,7 +20,7 @@ const write = root.write;
 const writeLog = std.debug.print;
 const Board = root.Board;
 
-const VERSION_STRING = "1.6.15";
+const VERSION_STRING = "1.6.16";
 
 pub fn main() !void {
     root.init();
