2ba05519de1e197132d823dad36d38f2bca83146
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-06-02 19:36:45 +0200

    remove old files bench: 3212514

--- datagen.sh
+++
@@ -1,7 +0,0 @@
-echo $1
-zig build-exe -OReleaseFast -fomit-frame-pointer -mcpu=znver5 src/datagen.zig
-for i in $(seq 1 $1); do
-    echo starting $i
-    ./datagen -o out$i.vf -g 3000_000 -n 5000 -r 6 &
-done
-wait
