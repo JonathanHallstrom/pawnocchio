21e5e77a6f7a39cb1a3940d66e6cc1c73358efc8
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-06-09 12:23:22 +0200

    update datagen config
    bench: 3288899

--- src/Searcher.zig
+++ src/Searcher.zig
@@ -1077,6 +1077,7 @@ pub fn startSearch(self: *Searcher, params: Params, is_main_thread: bool, quiet:
     var completed_depth: i32 = 0;
     for (1..MAX_PLY) |d| {
         const depth: i32 = @intCast(d);
+        self.limits.root_depth = depth;
         const assert = std.debug.assert;
         _ = &assert;
         var quantized_window: i64 = tunable_constants.aspiration_initial;
