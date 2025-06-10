b4dc90872545c0dabcc259290249ec567ff7df44
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-06-11 00:27:20 +0200

    lmr less in ttpv nodes
    bench: 4116808

--- src/Searcher.zig
+++ src/Searcher.zig
@@ -829,6 +829,8 @@ fn search(
                 reduction -= @intCast(history_lmr_mult * history_score >> 13);
                 reduction -= @intCast(tunable_constants.lmr_corrhist_mult * corrhists_squared >> 32);
                 reduction += tunable_constants.lmr_ttmove_mult * @intFromBool(has_tt_move);
+                reduction -= @as(i32, 1024) * @intFromBool(tt_pv);
+
                 reduction >>= 10;
 
                 const clamped_reduction = std.math.clamp(reduction, 1, depth - 1);
