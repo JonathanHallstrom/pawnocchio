6df264f14d2ccba7d1b36afeb816828014221100
JonathanHallstrom <lmj.hallstrom@gmail.com>
2025-06-12 14:03:42 +0200

    decrease minimum SE depth even more
    bench: 5114781

--- src/Searcher.zig
+++ src/Searcher.zig
@@ -772,7 +772,7 @@ fn search(
 
         var extension: i32 = 0;
         if (!is_root and
-            depth >= 6 and
+            depth >= 5 and
             move == tt_entry.move and
             !is_singular_search and
             tt_entry.depth + 3 >= depth and
