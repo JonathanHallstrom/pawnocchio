ea4dc15a3d28264af78a281fb60d66a9fe8a2d28
JonathanHallstrom <lmj.hallstrom@gmail.com>
2025-07-25 11:56:27 +0200

    wider
    bench: 5335924

--- src/Searcher.zig
+++ src/Searcher.zig
@@ -897,7 +897,7 @@ fn search(
                 if (s_score < s_beta - (tunable_constants.singular_dext_margin + if (is_pv) 20 else 0)) {
                     extension += 1;
 
-                    if (s_score < s_beta - (@as(i32, 100) + if (is_pv) 50 else 0)) {
+                    if (s_score < s_beta - (@as(i32, 100) + if (is_pv) 100 else 0)) {
                         extension += 1;
                     }
                 }
