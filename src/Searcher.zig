91fe917d36e99178caaa80b726ac2bc3a8d349ad
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-07-31 23:22:09 +0200

    add is_in_check to factorized
    bench: 5178670

--- src/Searcher.zig
+++ src/Searcher.zig
@@ -940,7 +940,7 @@ fn search(
                 var reduction = calculateBaseLMR(depth, num_searched, is_quiet);
                 reduction -= @intCast(history_lmr_mult * history_score >> 13);
                 reduction -= @intCast(tunable_constants.lmr_corrhist_mult * corrhists_squared >> 32);
-                reduction += lmrConvolve(8, .{
+                reduction += lmrConvolve(9, .{
                     is_pv,
                     cutnode,
                     improving,
@@ -949,6 +949,7 @@ fn search(
                     is_quiet,
                     gives_check,
                     self.stackEntry(-1).failhighs > 2,
+                    is_in_check,
                 });
 
                 reduction >>= 10;
