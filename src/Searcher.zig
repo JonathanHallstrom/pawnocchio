b54a037b65c821f0abd591e726de3de6d7afb504
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-08-02 23:50:38 +0200

    dont triple ext noisy moves
    bench: 4312862

--- src/Searcher.zig
+++ src/Searcher.zig
@@ -893,7 +893,7 @@ fn search(
                 if (s_score < s_beta - (tunable_constants.singular_dext_margin + if (is_pv) tunable_constants.singular_dext_pv_margin else 0)) {
                     extension += 1;
 
-                    if (!is_pv and s_score < s_beta - tunable_constants.singular_text_margin) {
+                    if (!is_pv and is_quiet and s_score < s_beta - tunable_constants.singular_text_margin) {
                         extension += 1;
                     }
                 }
