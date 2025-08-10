ae20cbf92719c6577b2bee6c535925db2107d07d
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-08-10 10:39:59 +0200

    use history score in see pruning for quiets
    bench: 4983229

--- src/Searcher.zig
+++ src/Searcher.zig
@@ -864,7 +864,7 @@ fn search(
             }
 
             const see_pruning_thresh = if (is_quiet)
-                tunable_constants.see_quiet_pruning_mult * lmr_depth
+                tunable_constants.see_quiet_pruning_mult * lmr_depth + @divTrunc(history_score, 200)
             else
                 tunable_constants.see_noisy_pruning_mult * lmr_depth * lmr_depth;
 
