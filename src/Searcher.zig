dfd4f68302ab76b002053207c257545a153b5711
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-09-11 10:48:22 +0200

    less aggro
    bench: 4486839

--- src/Searcher.zig
+++ src/Searcher.zig
@@ -755,7 +755,7 @@ fn search(
                 tunable_constants.rfp_worsening_margin * @intFromBool(opponent_worsening) -
                 tunable_constants.rfp_cutnode_margin * @intFromBool(no_tthit_cutnode) +
                 (corrplexity * tunable_constants.rfp_corrplexity_mult >> 32) -
-                @divTrunc(tt_move_hist, 64))
+                @divTrunc(tt_move_hist, 128))
         {
             return @intCast(eval + @divTrunc((beta - eval) * tunable_constants.rfp_fail_medium, 1024));
         }
