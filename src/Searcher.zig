1f7bb13a031061cb3251d5427bbb8f211837cf84
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-09-16 23:24:07 +0200

    ensure we're not in a singular search
    bench: 4544750

--- src/Searcher.zig
+++ src/Searcher.zig
@@ -737,7 +737,7 @@ fn search(
     }
     const eval = cur.static_eval;
 
-    if (!is_root and self.stackEntry(-1).board.checkers == 0 and !cur.move_is_noisy and !is_in_check) {
+    if (!is_root and self.stackEntry(-1).board.checkers == 0 and !cur.move_is_noisy and !is_in_check and !is_singular_search) {
         const previous_eval = self.stackEntry(-1).static_eval;
         if (previous_eval != evaluation.inf_score and eval != evaluation.inf_score) {
             const update = std.math.clamp(-5 * (previous_eval + eval), -100, 100);
