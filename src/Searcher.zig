0f3272c1297170dbe2a6675adc8ee79920335af7
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-10-10 15:19:28 +0200

    split triple ext margin by quiet
    bench: 13872948

--- src/Searcher.zig
+++ src/Searcher.zig
@@ -976,7 +976,7 @@ fn search(
                 if (s_score < s_beta - (tunable_constants.singular_dext_margin + if (is_pv) tunable_constants.singular_dext_pv_margin else 0)) {
                     extension += 1;
 
-                    if (!is_pv and s_score < s_beta - tunable_constants.singular_text_margin) {
+                    if (!is_pv and s_score < s_beta - tunable_constants.singular_text_margin - @as(i32, if (is_quiet) 30 else 0)) {
                         extension += 1;
                     }
                 }
