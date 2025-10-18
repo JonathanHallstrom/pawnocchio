60a62bc592b4eb287ffff09ef56645493142e8aa
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-10-19 01:06:59 +0200

    wider margin in pv nodes
    bench: 5553771

--- src/Searcher.zig
+++ src/Searcher.zig
@@ -934,10 +934,14 @@ fn search(
                 }
             }
 
-            const see_pruning_thresh = if (is_quiet)
+            var see_pruning_thresh = if (is_quiet)
                 @as(i64, tunable_constants.see_quiet_pruning_mult) * lmr_depth >> 10
             else
                 @as(i64, tunable_constants.see_noisy_pruning_mult) * lmr_depth * lmr_depth >> 20;
+
+            if (is_pv) {
+                see_pruning_thresh -= 100;
+            }
 
             if (!skip_see_pruning and
                 !SEE.scoreMove(board, move, @intCast(see_pruning_thresh), .pruning))
