a15f0280b6aad482c9f38b2cdf6f356c8b469f76
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-10-18 02:05:30 +0200

    wooo bad noisy futility pruning again
    bench: 13411152

--- src/Searcher.zig
+++ src/Searcher.zig
@@ -932,6 +932,22 @@ fn search(
                     mp.skip_quiets = true;
                     continue;
                 }
+            } else {
+                const futility_value = eval +
+                    tunable_constants.fp_base +
+                    @divTrunc(lmr_depth * tunable_constants.fp_mult, 1024) +
+                    SEE.value(board.pieceOn(move.to()) orelse .king, .pruning);
+                if (mp.stage == .bad_noisies and
+                    !is_in_check and
+                    lmr_depth <= 6104 and
+                    @abs(alpha) < 2000 and
+                    futility_value <= alpha)
+                {
+                    if (!evaluation.isTBScore(best_score)) {
+                        best_score = @intCast(@max(best_score, futility_value));
+                    }
+                    break;
+                }
             }
 
             const see_pruning_thresh = if (is_quiet)
