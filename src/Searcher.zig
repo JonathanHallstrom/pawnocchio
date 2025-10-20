c69c8b27508a4aa6922ac84a17eb009dad8c1890
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-10-20 23:54:43 +0200

    when tt entry indicates we're not gonna fail low reduce less in ttpv
    bench: 5696040

--- src/Searcher.zig
+++ src/Searcher.zig
@@ -1052,6 +1052,7 @@ fn search(
                 var reduction = calculateBaseLMR(depth, num_searched, is_quiet);
                 reduction -= @intCast(history_lmr_mult * history_score >> 13);
                 reduction -= @intCast(tunable_constants.lmr_corrhist_mult * corrhists_squared >> 32);
+                reduction -= @as(i32, 512) * @intFromBool(tt_hit and tt_pv and tt_entry.score > alpha);
                 reduction += getFactorisedLmr(8, .{
                     is_pv,
                     cutnode,
