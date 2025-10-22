9dcee3fc4808071c61321ac61db6292ea21741bf
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-10-22 14:37:15 +0200

    dont allow rfp in ttpv nodes
    bench: 4018835

--- src/Searcher.zig
+++ src/Searcher.zig
@@ -763,16 +763,17 @@ fn search(
         // basically we reduce more if this node is likely unimportant
         const no_tthit_cutnode = !tt_hit and cutnode;
         const opponent_has_easy_capture = board.occupancyFor(stm) & board.lesser_threats[stm.flipped().toInt()] != 0;
-        if (eval >= beta +
-            tunable_constants.rfp_base +
-            tunable_constants.rfp_mult * depth +
-            tunable_constants.rfp_quad * depth * depth -
-            tunable_constants.rfp_improving_margin * @intFromBool(improving) -
-            tunable_constants.rfp_improving_easy_margin * @intFromBool(improving and !opponent_has_easy_capture) -
-            tunable_constants.rfp_easy_margin * @intFromBool(opponent_has_easy_capture) -
-            tunable_constants.rfp_worsening_margin * @intFromBool(opponent_worsening) -
-            tunable_constants.rfp_cutnode_margin * @intFromBool(no_tthit_cutnode) +
-            (corrplexity * tunable_constants.rfp_corrplexity_mult >> 32))
+        if (!tt_pv and
+            eval >= beta +
+                tunable_constants.rfp_base +
+                tunable_constants.rfp_mult * depth +
+                tunable_constants.rfp_quad * depth * depth -
+                tunable_constants.rfp_improving_margin * @intFromBool(improving) -
+                tunable_constants.rfp_improving_easy_margin * @intFromBool(improving and !opponent_has_easy_capture) -
+                tunable_constants.rfp_easy_margin * @intFromBool(opponent_has_easy_capture) -
+                tunable_constants.rfp_worsening_margin * @intFromBool(opponent_worsening) -
+                tunable_constants.rfp_cutnode_margin * @intFromBool(no_tthit_cutnode) +
+                (corrplexity * tunable_constants.rfp_corrplexity_mult >> 32))
         {
             return @intCast(eval + @divTrunc((beta - eval) * tunable_constants.rfp_fail_medium, 1024));
         }
