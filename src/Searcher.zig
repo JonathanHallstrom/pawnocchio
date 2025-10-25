6f19c3f2761fdaacf427b2af47a2870f8e304ea7
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-10-26 00:22:39 +0200

    reduce quiets more if ttmove is noisy
    bench: 4128482

--- src/Searcher.zig
+++ src/Searcher.zig
@@ -673,7 +673,7 @@ fn search(
     const has_tt_move = tt_hit and !tt_entry.move.isNull();
     const tt_pv = is_pv or (tt_hit and tt_entry.flags.is_pv);
     const tt_score = evaluation.scoreFromTt(tt_entry.score, self.ply);
-    // const tt_move_quiet = has_tt_move and board.isQuiet(tt_entry.move);
+    const tt_move_quiet = has_tt_move and board.isQuiet(tt_entry.move);
     // const tt_move_hist = if (has_tt_move) if (tt_move_quiet) self.histories.readQuietPruning(board, tt_entry.move, self.getUsableMoves()) else self.histories.readNoisy(board, tt_entry.move) else 0;
     if (tt_hit) {
         if (tt_entry.depth >= depth and !is_singular_search) {
@@ -1044,6 +1044,9 @@ fn search(
                 var reduction = calculateBaseLMR(depth, num_searched, is_quiet);
                 reduction -= @intCast(history_lmr_mult * history_score >> 13);
                 reduction -= @intCast(tunable_constants.lmr_corrhist_mult * corrhists_squared >> 32);
+                if (is_quiet and has_tt_move and !tt_move_quiet) {
+                    reduction += 512;
+                }
                 reduction += getFactorisedLmr(8, .{
                     is_pv,
                     cutnode,
