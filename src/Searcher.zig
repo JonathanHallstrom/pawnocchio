e52d956193b528711a2a3f1f00093e98bf099cad
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-11-11 19:47:54 +0100

    make dodeeper condition simpler
    bench: 4123566

--- src/Searcher.zig
+++ src/Searcher.zig
@@ -1099,7 +1099,7 @@ fn search(
 
                 if (s > alpha and reduced_depth < new_depth) {
                     const do_deeper_search = s > best_score + tunables.lmr_dodeeper_margin + tunables.lmr_dodeeper_mult * new_depth;
-                    const do_shallower_search = s < best_score + new_depth;
+                    const do_shallower_search = s < best_score + 5;
 
                     new_depth += @intFromBool(do_deeper_search);
                     new_depth -= @intFromBool(do_shallower_search);
