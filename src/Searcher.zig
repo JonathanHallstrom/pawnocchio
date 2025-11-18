a9a8beba9fe6fbad8e5459283e9c323cb66e1f04
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-11-18 08:06:39 +0100

    allocate searchers separately
    bench: 4051056

--- src/Searcher.zig
+++ src/Searcher.zig
@@ -1249,7 +1249,7 @@ fn writeInfo(self: *Searcher, score: i16, depth: i32, tp: InfoType) void {
     };
     var nodes: u64 = 0;
     var tbhits: u64 = 0;
-    for (engine.searchers) |*searcher| {
+    for (engine.searchers) |searcher| {
         nodes += searcher.nodes;
         tbhits += searcher.tbhits;
     }
@@ -1463,7 +1463,7 @@ pub fn startSearch(self: *Searcher, params: Params, is_main_thread: bool, quiet:
             var move_stability: u32 = 0;
             var best_move_count: u64 = 0;
             var total_nodes: u64 = 0;
-            for (engine.searchers) |*searcher| {
+            for (engine.searchers) |searcher| {
                 eval_stability += searcher.eval_stability;
                 if (searcher.root_move == self.root_move) {
                     move_stability += searcher.move_stability;
