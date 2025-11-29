bb72e1bff8f5c7f48067cf45fc4f8b6ab6b3b0a0
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-11-29 12:43:11 +0100

    add small script that install toolchain and builds
    bench: 3435886

---
+++ build_from_scratch.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+curl https://raw.githubusercontent.com/tristanisham/zvm/master/install.sh | bash
+
+VERSION=0.15.2
+$HOME/.zvm/self/zvm i $VERSION
+$HOME/.zvm/$VERSION/zig build --release=fast
