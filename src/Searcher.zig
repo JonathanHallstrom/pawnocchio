fcad99ac4828a32a566f3cc077aa36d6596f5f31
Jonathan Hallström <lmj.hallstrom@gmail.com>
2026-04-06 20:50:48 +0200

    dont allow razoring when we have a quiet tt move
    bench: 4827710

--- src/Searcher.zig
+++ src/Searcher.zig
@@ -936,11 +936,13 @@ fn search(
         // razoring
         const we_have_easy_capture = board.occupancyFor(stm.flipped()) & (&board.lesser_threats)[stm.toInt()] != 0;
         const depth_3 = @max(0, depth - 3);
-        if (eval +
-            TUNABLES.razoring_offs +
-            TUNABLES.razoring_mult * depth +
-            TUNABLES.razoring_quad * depth_3 * depth_3 +
-            TUNABLES.razoring_easy_capture * @intFromBool(we_have_easy_capture) <= alpha)
+        const tt_move_is_quiet = has_tt_move and board.isQuiet(tt_entry.move);
+        if (!tt_move_is_quiet and
+            eval +
+                TUNABLES.razoring_offs +
+                TUNABLES.razoring_mult * depth +
+                TUNABLES.razoring_quad * depth_3 * depth_3 +
+                TUNABLES.razoring_easy_capture * @intFromBool(we_have_easy_capture) <= alpha)
         {
             const razor_score = if (is_tt_corrected_eval) eval else self.qsearch(
                 is_root,
