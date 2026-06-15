d5563b5934cec8f603d4a24853bdc6b31a301ccf
Jonathan Hallström <lmj.hallstrom@gmail.com>
2026-06-15 13:32:08 +0200

    update wdl model
    bench: 2361275

--- src/wdl.zig
+++ src/wdl.zig
@@ -21,27 +21,11 @@ const root = @import("root.zig");
 pub fn wdlParams(material: anytype) struct { f64, f64 } {
     const x: f64 = @floatFromInt(material);
 
-    // p_a = ((-5.273 * x / 58 + -3.581) * x / 58 + 2.813) * x / 58 + 284.631
-    // p_b = ((49.231 * x / 58 + -69.854) * x / 58 + 48.236) * x / 58 + 41.310
-    //     constexpr double as[] = {-5.27254245, -3.58054810, 2.81336795, 284.63146557};
-    //     constexpr double bs[] = {49.23056003, -69.85434163, 48.23555930, 41.30969488};
-
-    const p_a = ((-5.273 * x / 58 + -3.581) * x / 58 + 2.813) * x / 58 + 284.631;
-    const p_b = ((49.231 * x / 58 + -69.854) * x / 58 + 48.236) * x / 58 + 41.310;
+    const p_a = ((-43.652 * x / 58 + 115.844) * x / 58 + -135.616) * x / 58 + 337.002;
+    const p_b = ((43.328 * x / 58 + -55.082) * x / 58 + 29.134) * x / 58 + 54.813;
 
     return .{ p_a, p_b };
 }
-
-// std::pair<i32, i32> wdlModel(Score povScore, i32 material) {
-//     const auto [a, b] = wdlParams(material);
-//
-//     const auto x = static_cast<f64>(povScore);
-//
-//     return {
-//         static_cast<i32>(std::round(1000.0 / (1.0 + std::exp((a - x) / b)))),
-//         static_cast<i32>(std::round(1000.0 / (1.0 + std::exp((a + x) / b))))
-//     };
-// }
 
 pub fn wdlModel(score: i16, material: anytype) struct { i32, i32, i32 } {
     const a, const b = wdlParams(material);
